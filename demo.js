30e6f9d3d3b41bd36961ab520dc9fa662ec3028c
Sai Pavithra Akkulappagari <121482088+SaiPavithra-Akkulappagari@users.noreply.github.com>
2022-12-27 13:37:16 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,15 @@
+function dap() {
+    alert("Your order placed Successfully");
+}
+
+function instagram() {
+    window.location.href = 'https://www.instagram.com/archna._.royl/';
+}
+
+function facebook() {
+    window.location.href = 'https://www.facebook.com/archana.patukuri.9';
+}
+
+function twitter() {
+    window.location.href = 'https://www.twitter.com/archnapatukuri/';
+}
